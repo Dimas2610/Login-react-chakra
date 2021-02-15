73e9509366e05ba79db3cbf8df5ea38c9379b1ca
Dimas <dianlaru10@gmail.com>
2021-02-15 17:37:55 -0400

    basic login not functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  ThemeProvider,
+  theme,
+  ColorModeProvider,
+  CSSReset,
+  Box,
+  Flex,
+  IconButton,
+  useColorMode,
+  Heading,
+  Text,
+  Link,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Checkbox,
+  Button
+} from '@chakra-ui/react';
+import { AccordionButton } from "@chakra-ui/react";
+import { PhoneIcon, SunIcon } from '@chakra-ui/icons';
+const VARIANT_COLOR = 'teal'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <ColorModeProvider options = { { 
+        useSystsemColorMode : true 
+      } } >
+        <CSSReset />
+        <LoginArea />
+      </ColorModeProvider>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+const LoginArea = () => {
+  return (
+    <Flex minHeight='100vh' width='full' align='center' justifyContent='center'>
+      <Box 
+        borderWidth={1}
+        px={4}
+        width='full'
+        maxWidth='400px'
+        borderRadius={10}
+        textAlign='center'
+        boxShadow='lg'
+      >
+      <Box p={5}>
+          <LoginHeader />
+          <LoginForm />
+        </Box>
+      </Box>
+    </Flex>
+  )
+}
+
+const LoginHeader = () => {
+  return (
+    <Box textAlign='center'>
+      <Heading>LOGIN</Heading>
+    </Box>
+  )
+}
+
+const LoginForm = () => {
+  return (
+    <Box my={4} textAlign='left'>
+      <form>
+
+        <FormControl>
+          <FormLabel>Usuario</FormLabel>
+          <Input type='text' placeholder='Introduzca su usuario' />
+        </FormControl>
+
+        <FormControl mt={4}>
+          <FormLabel>Contraseña</FormLabel>
+          <Input type='password' placeholder='Introduzca su contraseña' />
+        </FormControl>
+        
+        <Stack>
+          <Button colorScheme="blue" mt={5} >Sign In</Button>
+        </Stack>
+        
+        
+       
+        <Stack isInline justifyContent='space-between' mt={4}>
+            <Box>
+              <Checkbox>Remember Me</Checkbox>
+            </Box>
+            <Box>
+              <Link color={`${VARIANT_COLOR}.500`}>Forgot your password?</Link>
+            </Box>
+        </Stack>
+        <Stack>
+          <Box>
+            <Checkbox>Not a member?</Checkbox>
+          </Box>
+        </Stack>
+      </form>
+    </Box>
+  )
+}
+
+export default App
